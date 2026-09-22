6fb85528734a81ed0515642dafa4be4cee98a978
Naveen20058 <20005k.naveen@gmail.com>
2026-09-22 06:13:30 +0530

    update

---
+++ java script/Day1/task1/index.js
@@ -0,0 +1,6 @@
+let Name = " mohit";
+console.log(Name);
+let name = "manohar";
+console.log(name);
+let age = 22;
+console.log(age);
---
+++ java script/Day1/task2/m.js
@@ -0,0 +1,4 @@
+let number = 20;
+console.log(number);
+let age = 22;
+console.log(age);
