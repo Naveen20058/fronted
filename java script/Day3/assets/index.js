9ee1a2d94013bce06680f06e69b357d5986645d3
Naveen20058 <20005k.naveen@gmail.com>
2026-09-24 20:47:08 +0530

    update

---
+++ java script/Day3/assets/index.js
@@ -0,0 +1,44 @@
+let shirtPrice = 1200;
+let quantity = 3;
+let deliveryCharge = 100;
+
+let productTotal = shirtPrice * quantity;
+let finalBill = productTotal + deliveryCharge;
+
+console.log("Product Total:", productTotal);
+console.log("Final Bill:", finalBill);
+
+
+let salary = 30000;
+let bonus = 5000;
+
+salary += bonus;
+
+let tax = 2000;
+salary -= tax;
+
+console.log("Final Salary:", salary);
+
+
+let balance = 20000;
+let deposit = 5000;
+let withdrawal = 3000;
+
+balance += deposit;
+balance -= withdrawal;
+
+console.log("Final Balance:", balance);
+
+
+
+let tamil = 80;
+let english = 75;
+let maths = 90;
+let science = 85;
+let computer = 95;
+
+let totalMarks = tamil + english + maths + science + computer;
+let averageMarks = totalMarks / 5;
+
+console.log("Total Marks:", totalMarks);
+console.log("Average Marks:", averageMarks);
---
+++ java script/Day4/assets/index.js
@@ -0,0 +1,31 @@
+////var a = 100
+//for (var a = 0; a <= 100; a++) {
+//    console.log(a);
+
+//}
+var milk = 30
+var bisket = 20
+var icecream = 40;
+var chocolote = 10;
+
+if (milk >= 30) {
+    console.log("milk");
+
+}
+else if (bisket >= 20) {
+    console.log("bisket");
+
+
+}
+else if (icecream >= 40) {
+    console.log("icecream");
+
+}
+else if (chocolote >= 10) {
+    console.log("chocolote");
+
+}
+else {
+    console.log("you not give any item");
+
+}
